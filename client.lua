128b9995d16c42fe585511d55ab2193525582dde
WolveCommunity <113908190+WolveCommunity@users.noreply.github.com>
2022-09-19 15:14:00 +0200

    Delete client.lua

--- client.lua
+++
@@ -1,5 +0,0 @@
-RegisterCommand("announce", function(source, args)
-    TriggerServerEvent('announce', table.concat(args, " "))
-     -- we have to concatenate the table because the 'args' cb return a table (array)
-     -- the 2nd parameter in 'table.concat' is just spacing each args out
-end)
